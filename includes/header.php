da2437a111c023b5857bedc7efde55ad6662e3a0
Sushmitha Shetty <90413690+SushmithaShetty180@users.noreply.github.com>
2021-09-21 18:32:12 +0530

    Update header.php

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,20 @@
-
+<nav class="navbar navbar-fixed-top " style="background-color:DCDCDC;">
+<div class="container">
+<div class="navbar-header">
+<button class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+<span class="icon-bar"></span>
+</button>
+<a class="navbar-brand" style="color: magenta;">The GRIPS Bank</a>
+</div>
+<div class="collapse navbar-collapse" id="mynavbar">
+<ul class="nav navbar-nav navbar-right" >
+<li><a href = "index.php" style="color: mediumblue;"><span class = "glyphicon glyphicon-home"></span> Home</a></li>
+<li><a href="user.php" style="color: mediumblue;"><span class="glyphicon glyphicon-user"></span> New Users</a></li>
+<li><a href="transaction.php" style="color: mediumblue;"><span class="glyphicon glyphicon-transfer"></span> Transfer Money</a></li>
+<li><a href="history.php" style="color: mediumblue;"><span class="glyphicon glyphicon-inbox"></span> History</a></li>
+</ul>
+</div>
+</div>
+</nav>
