9f141a6e8835c49bebc29458d8937bc7a878869a
Sushmitha Shetty <90413690+SushmithaShetty180@users.noreply.github.com>
2021-09-21 18:31:37 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,1 +1,22 @@
-
+<?php
+include 'includes/common.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta http-equiv="X-UA-Compatible" content="ie=edge" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="css/style.css">
+<title>The GRIPS Bank</title>
+</head>
+<body>
+<footer class="text-center" style="background-color: #FFE4C4; color: blue;">
+<p>&copy 2021. Made by <b> Sushmitha Shetty </b> <br> Basic Banking System project for <b>The Sparks Foundation</b></p>
+</footer>
+</body>
+</html>
