a13acc0302abc8d12ada37243f96696bdfb773d4
Sushmitha Shetty <90413690+SushmithaShetty180@users.noreply.github.com>
2021-09-21 18:24:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,38 @@
+<?php
+include 'includes/common.php';
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!--Latest compiled and minified JavaScript--> 
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>The GRIPS Bank</title>
+</head>
+<style>
+body{
+          background-color:lavenderblush;
+          background-image:url("https://thumbs.dreamstime.com/z/bank-credit-card-neon-icon-elements-banking-set-simple-icon-websites-web-design-mobile-app-info-graphics-dark-144330611.jpg");
+            }
+.center {
+  text-align: center;
+}
+</style>
+
+<body>
+<?php
+include 'includes/header.php';
+?>
+<div class=".content">
+</div>
+<?php
+include 'includes/footer.php';
+?>
+</body>
+</html>
